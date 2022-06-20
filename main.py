10fbc655493d3776a096f70c90221362095c2b3f
Daniel <juan.vazquez@globant.com>
2022-06-20 14:34:58 -0500

    Starting code

---
+++ main.py
@@ -0,0 +1,8 @@
+
+import argparse
+import time
+from datetime import datetime
+from pynput.mouse import Controller as MouseController
+from pynput.keyboard import Key, Controller as KeyboardController
+
+mouse = MouseController()
